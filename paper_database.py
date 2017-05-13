299eef3b3c227db7121c998a5d4d9b3b26fee18a
mengyujackson121 <mengyujackson121@gmail.com>
2017-05-12 22:10:21 -0400

    pass test

--- paper_database.py
+++ paper_database.py
@@ -1,6 +1,27 @@
-def add_paper(*args, **kwargs):
-    pass
+import peewee
+from peewee import *
+db = MySQLDatabase('mengyu', user='root',passwd='MySQLpassword')    # create a connection with database, setup username and password
 
 
-def get_papers_by_author(*args, **kwargs):
-    pass
+#function get author and title from test_file, save in table: paper
+def add_paper(author, title):
+    paper = Paper(author=author, title=title)
+    paper.save()
+
+#found out all paper by the author
+def get_papers_by_author(author):
+    list_paper = []                                                     #list to store
+    for book in Paper.filter(author=author):
+        list_paper.append(book)
+    return list_paper
+
+#peewee magic talk to database
+class Paper(peewee.Model):
+    author = peewee.CharField()
+    title = peewee.TextField()
+
+    class Meta:
+        database = db
+
+
+
