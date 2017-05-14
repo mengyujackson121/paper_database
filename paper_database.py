4bd064614a41d9fa0f9f0006b81322c82c5fad61
stevenjackson121 <stevenjackson121@gmail.com>
2017-05-14 16:00:03 -0400

    Actually, we want to store information about authors in a separate table. We should use the author's ID in the paper table, not just a name, since more than one author might have the same name!

--- paper_database.py
+++ paper_database.py
@@ -1,27 +1,48 @@
 import peewee
 from peewee import *
 db = MySQLDatabase('mengyu', user='root',passwd='MySQLpassword')    # create a connection with database, setup username and password
+db.connect()
 
 
-#function get author and title from test_file, save in table: paper
-def add_paper(author, title):
-    paper = Paper(author=author, title=title)
-    paper.save()
-
-#found out all paper by the author
-def get_papers_by_author(author):
-    list_paper = []                                                     #list to store
-    for book in Paper.filter(author=author):
-        list_paper.append(book)
-    return list_paper
-
-#peewee magic talk to database
+# peewee magic talk to database
 class Paper(peewee.Model):
     author = peewee.CharField()
     title = peewee.TextField()
 
     class Meta:
         database = db
+
+
+# FIXME
+class Author(peewee.Model):
+    class Meta:
+        database = db
+
+
+db.create_tables([Paper], safe=True)
+
+
+# Add new author with the given fields
+# FIXME
+def add_author(title=None, first_name=None, middle_name=None, last_name=None):
+    pass
+
+
+#function get author and title from test_file, save in table: paper
+# FIXME
+def add_paper(author_id, title):
+    paper = Paper(author=author, title=title)
+    paper.save()
+
+
+# found out all paper by the author
+# FIXME
+def get_papers_by_author(author):
+    list_paper = []                                                     #list to store
+    for book in Paper.filter(author=author):
+        list_paper.append(book)
+    return list_paper
+
 
 
 
--- test_file.py
+++ test_file.py
@@ -1,14 +1,24 @@
 import paper_database
 
 
-def create_paper_test():
-    paper_database.add_paper(author="steven jackson", title="foo")
-    papers = paper_database.get_papers_by_author(author="steven jackson")
-    assert papers[0].title == "foo"
+def create_paper_test(id_1, id_2):
+    paper_database.add_paper(author_id=id_1, title="Steven's Masterpiece")
+    paper_database.add_paper(author_id=id_2, title="Mengyu's Even Better Book!")
+    papers_1 = paper_database.get_papers_by_author(id_1)
+    assert papers_1[0].title == "Steven's Masterpiece"
+    papers_2 = paper_database.get_papers_by_author(id_2)
+    assert papers_2[0].title == "Mengyu's Even Better Book!"
 
+
+def create_author_test():
+    new_author = paper_database.add_author(title="Mr.", first_name="Steven", middle_name="Tecumseh",
+                                           last_name="Jackson")
+    new_author_2 = paper_database.add_author(title="Mrs.", first_name="Mengyu", middle_name="Bai", last_name="Jackson")
+    return new_author.id, new_author_2.id
 
 def main():
-    create_paper_test()
+    id_1, id_2 = create_author_test()
+    create_paper_test(id_1, id_2)
 
 
 if __name__ == '__main__':
