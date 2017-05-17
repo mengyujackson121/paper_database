cdd04e4a4a0726bc5254e63fc3b36b798d680269
mengyujackson121 <mengyujackson121@gmail.com>
2017-05-16 21:15:44 -0400

    pass 2nd test

--- paper_database.py
+++ paper_database.py
@@ -4,36 +4,38 @@ db = MySQLDatabase('mengyu', user='root',passwd='MySQLpassword')    # create a c
 db.connect()
 
 
-# peewee magic talk to database
-class Paper(peewee.Model):
-    author = peewee.CharField()
-    title = peewee.TextField()
-
+class BaseModel(Model):
     class Meta:
         database = db
 
+# peewee magic talk to database
 
 # FIXME
-class Author(peewee.Model):
-    class Meta:
-        database = db
+class Author(BaseModel):
+    title = peewee.CharField()
+    first_name = peewee.CharField()
+    middle_name = peewee.CharField()
+    last_name = peewee.CharField()
 
 
-db.create_tables([Paper], safe=True)
+class Paper(BaseModel):
+    author = ForeignKeyField(Author)
+    title = peewee.TextField()
 
-
+db.drop_tables([Author,Paper])
+db.create_tables([Paper,Author], safe=True)
 # Add new author with the given fields
 # FIXME
 def add_author(title=None, first_name=None, middle_name=None, last_name=None):
-    pass
-
+    author = Author(title=title, first_name=first_name, middle_name=middle_name, last_name=last_name)
+    author.save()
+    return author
 
 #function get author and title from test_file, save in table: paper
 # FIXME
 def add_paper(author_id, title):
-    paper = Paper(author=author, title=title)
+    paper = Paper(author=author_id, title=title)
     paper.save()
-
 
 # found out all paper by the author
 # FIXME
@@ -42,7 +44,6 @@ def get_papers_by_author(author):
     for book in Paper.filter(author=author):
         list_paper.append(book)
     return list_paper
-
 
 
 
